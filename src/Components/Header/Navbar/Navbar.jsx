9fb8f317586761efe3f6712f2275bf72d311a222
ShoroveTajmen <sorovitajmen@gmail.com>
2023-09-24 22:27:40 +0600

    Navbar created and set dynamic routing

---
+++ src/Components/Header/Navbar/Navbar.jsx
@@ -0,0 +1,59 @@
+import { NavLink } from "react-router-dom";
+import Logo from "./Logo";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="flex justify-between items-center py-6 shadow-md px-5">
+        <Logo></Logo>
+
+        <ul className="flex gap-10 font-bold text-lg">
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive, isPending }) =>
+                isPending
+                  ? "pending"
+                  : isActive
+                  ? "text-[#FF444A] underline"
+                  : ""
+              }
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/donation"
+              className={({ isActive, isPending }) =>
+                isPending
+                  ? "pending"
+                  : isActive
+                  ? "text-[#FF444A] underline"
+                  : ""
+              }
+            >
+             Donation
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/statistics"
+              className={({ isActive, isPending }) =>
+                isPending
+                  ? "pending"
+                  : isActive
+                  ? "text-[#FF444A] underline"
+                  : ""
+              }
+            >
+              Statistics
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,11 @@
+
+
+const ErrorPage = () => {
+    return (
+        <div className="h-screen flex justify-center items-center text-3xl text-red-600">
+            Error!!!!!
+        </div>
+    );
+};
+
+export default ErrorPage;

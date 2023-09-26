9e56010f6bc4f79b0fa6e62448781f12410381c0
ShoroveTajmen <sorovitajmen@gmail.com>
2023-09-26 09:20:55 +0600

    Responsive done for all device

--- src/Components/Header/Banner/Banner.jsx
+++ src/Components/Header/Banner/Banner.jsx
@@ -29,15 +29,15 @@ const Banner = ({ data }) => {
 
         {/* <!-- White overlay with opacity --> */}
         <div className="absolute top-0 right-0 bottom-0 left-0 bg-white opacity-90"></div>
-        <div className="relative ml-[19px] md:ml-[400px]">
+        <div className="relative ml-[19px] md:ml-[160px] lg:ml-[400px]">
           <div className="absolute mt-[220px]  md:mt-[200px]">
-            <h1 className=" text-black text-2xl md:text-5xl font-bold mb-[10px] md:mb-[40px]">
+            <h1 className=" text-black text-2xl md:text-3xl lg:text-5xl font-bold mb-[10px] md:mb-[40px]">
               I Grow By Helping People In Need
             </h1>{" "}
             <br />
             <div className="text-center">
               <input
-                className="w-[250px]  md:ml-5 md:w-[470px] h-[50px] bg-white rounded-l-lg border-solid border border-[#DEDEDE] pl-4 "
+                className="w-[250px]  md:ml-2 lg:ml-5 md:w-[360px] lg:w-[470px] h-[50px] bg-white rounded-l-lg border-solid border border-[#DEDEDE] pl-4 "
                 type="text"
                 placeholder="Search here..."
                 value={searchInput}
@@ -54,7 +54,7 @@ const Banner = ({ data }) => {
         </div>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-4  md:gap-5 w-full md:w-[1400px] mx-auto mt-[30px] md:mt-[70px]">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4  lg:gap-5 w-full md:w-[700px] lg:w-[1400px] mx-auto mt-[30px] md:mt-[70px]">
         {filteredData.map((item) => (
           <DataCards key={item.id} data={item} /> // Pass filtered data to DataCards component
         ))}
--- src/Pages/Donation/DonationCard.jsx
+++ src/Pages/Donation/DonationCard.jsx
@@ -27,13 +27,13 @@ const DonationCard = ({ card }) => {
         }}
       >
         <img
-          className=" w-[220px] rounded-t-lg h-[150px] md:h-[200px] rounded-lg"
+          className=" w-[220px] md:w-[200px] lg:w-[220px] rounded-t-lg h-[150px] md:h-[200px] rounded-lg"
           src={picture}
           alt=""
         />
-        <div className="flex flex-col justify-between p-4 h-[190px] ">
+        <div className="flex flex-col justify-between p-4 h-[190px] md:h-[220px] lgg:h-[220px] ">
           <h5
-            className="mb-2 p-1 w-[100px] rounded text-center"
+            className="mb-2  p-1 w-[100px] rounded text-center"
             style={{
               backgroundColor: `${category_background_color}`,
               color: `${text_color}`,
@@ -41,7 +41,7 @@ const DonationCard = ({ card }) => {
           >
             {category}
           </h5>
-          <p className=" text-lg md:text-2xl font-bold text-black dark:text-gray-400">
+          <p className=" text-lg md:text-lg lg:text-2xl font-bold text-black dark:text-gray-400">
             {title}
           </p>
           <h3 className="font-semibold" style={{ color: `${text_color}` }}>
@@ -49,7 +49,7 @@ const DonationCard = ({ card }) => {
           </h3>
             <button
             onClick={()=>navigate(`/cards/${id}`)}
-              className="text-white font-semibold w-[120px] p-1 md:p-2 mt-2 rounded block"
+              className="text-white font-semibold w-[120px] p-1 lg:p-2 mt-2 md:mt-0 rounded block"
               style={{
                 backgroundColor: `${text_color}`,
               }}
